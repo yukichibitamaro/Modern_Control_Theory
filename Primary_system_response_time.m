6ba6373f6e2e6960149a9dfaaa5de14f171ba5a4
YuyaShinagawa_DESKTOP_HOME <yuya4730@gmail.com>
2024-02-25 03:20:41 +0900

    線形n次システムの時間応答ソースコードの構築と1次システム時間応答のソースコードの編集

--- Primary_system_response_time.m
+++ Primary_system_response_time.m
@@ -1,4 +1,5 @@
 %% 任意の時間応答(ステップ応答)のサンプルコード
+%% 線形1次システム(RC直列回路)
 %% バージョン：MATLAB 2023b
 
 %% RESET
@@ -9,18 +10,18 @@ format compact;  % 余計な改行を消去
 %% SYMBOLIC VARIABLE DEFINITION
 syms R C E q0 t tau
 %% DEFINING STATE-SPACE MODEL
-a = -1/(R*C);
-b = 1/R;
-c = 1;
-d = 0;
-x0 = q0;
-u = E;
+a = -1/(R*C); % システム係数スカラー a
+b = 1/R;      % 入力係数スカラー b
+c = 1;        % 観測スカラー c
+d = 0;        % 直達スカラー d
+x0 = q0;      % 状態変数の初期値
+u = E;        % ステップ入力
 %% CALCULATING TIME RESPONSE
 % 零状態応答
 y1 = c * q0 * exp(a*t);
 % 零入力応答
 f = exp(a*(t-tau))*b*u;
 y2 = int(f,tau,[0,t]);
-% 時間応答
+% 時間応答の解析解の表示
 disp("ステップ応答の解析解")
 y = y1 + y2
--- n_Secondary_system_response_time.m
+++ n_Secondary_system_response_time.m
@@ -1,4 +1,5 @@
-%% 任意の時間応答(ステップ応答)のサンプルコード
+%% 任意の時間応答(単位ステップ応答)のサンプルコード
+%% 線形n次システム(1慣性系機械システム)
 %% バージョン：MATLAB 2023b
 
 %% RESET
@@ -7,20 +8,25 @@ close all;       % Figure消去
 clc;             % コマンドウィンドウの履歴消去
 format compact;  % 余計な改行を消去
 %% SYMBOLIC VARIABLE DEFINITION
-syms R C E q0 t tau
+syms m c k F t tau % シンボリック変数の定義
+m = 1;  % 質量係数
+c = 2;  % 粘性係数
+k = 10; % ばね定数
+F = 1;  % 一定値入力(単位入力)
 %% DEFINING STATE-SPACE MODEL
-a = -1/(R*C);
-b = 1/R;
-c = 1;
-d = 0;
-x0 = q0;
-u = E;
+A = [   0,    1 ;  % システム係数行列 A
+     -k/m, -c/m];
+B = [0; 1/m];      % 入力係数行列 B
+C = [1 0];         % 観測行列 C
+D = 0;             % 直達項 D
+x0 = zeros(2,1);   % 状態変数の初期値
+u = 1;             % 単位ステップ入力
 %% CALCULATING TIME RESPONSE
 % 零状態応答
-y1 = c * q0 * exp(a*t);
+y1 = C * expm(A*t)* x0;
 % 零入力応答
-f = exp(a*(t-tau))*b*u;
+f = C * expm(A*(t-tau))*B*u;
 y2 = int(f,tau,[0,t]);
-% 時間応答
-disp("ステップ応答の解析解")
+% 時間応答の解析解の表示
+disp("単位ステップ応答の解析解")
 y = y1 + y2
