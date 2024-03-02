fe84ab1b5fd240c1dc636aa99fe4a71de17390d3
YuyaShinagawa_DESKTOP_HOME <yuya4730@gmail.com>
2024-03-02 22:58:33 +0900

    積分型サーボ系のsimulinkとセットmファイルのアップロード マスばねダンパのアニメーション関数のアップロード

---
+++ Function_animate1MSD.m
@@ -0,0 +1,118 @@
+% k = 1のみに変更するひつようあり
+
+function Function_animate1MSD(Time, Displacement,Target,Control_Input,steps)
+    %% 初期設定
+    % 質点の設定
+    Nmass = 1; % 慣性体の数を設定
+    w = 8;     % 質点の幅の設定  ---> targetから生成されるように変更する必要あり
+    h = 2.2;   % 質点の高さの設定
+    y = 0;     % 位置の設定
+    % 変数設定
+    t = linspace(Time(1), Time(end), steps); % シミュレーション時間をアニメーションステップ数に分割して時間ベクトルを生成
+    x = zeros(steps, Nmass); % 質点の位置の空行列を定義
+    u = zeros(steps, Nmass); % 制御入力の空行列を定義
+    % ルックアップテーブルを使用して各質点の位置や入力をアニメーション時間に対して離散点を補間する
+    for k = 1:Nmass 
+        x(:, k) = interp1(Time, Displacement(:, k), t);  % 質点の位置 
+        u(:,k)  = interp1(Time, Control_Input(:, k), t); % 制御入力
+    end
+    spacing = fix(2 * max(abs(x), [], "all")); % 初期位置設定
+    remainder = mod(spacing,5);
+    difference = 5 - remainder;
+    if (remainder ~= 0)
+    spacing = spacing + difference;
+    end
+    xmax = spacing * 2; % x軸の範囲を設定
+    %% 動画の保存準備
+    filename = 'Animation.mp4'; % 保存する動画ファイル名
+    vidObj = VideoWriter(filename, 'MPEG-4'); % VideoWriterオブジェクトの作成
+    open(vidObj); % 動画ファイル起動 
+    %% アニメーションの描画
+    fig = figure;
+    set(fig, 'Position', get(0,'Screensize')); % ディスプレイに対してフルスクリーンでグラフ描画
+    for j = 1:numel(t)
+        % 質点の挙動(アニメーション)を表示するサブプロットを作成
+        subplot(3,1,1)
+        cla % サンプリングごとの慣性体,ばね,ダンパの履歴消去
+        hold on
+        x0k = 0; % ばね，ダンパの左端点の位置
+        for k = 1:Nmass
+            % ばねの描画
+            xc = spacing * k + x(j,k);     % 慣性体の位置(重心)設定       
+            xs = linspace(x0k,xc,spacing); % ばねのギザギザ具合の設定1
+            ys = 0.3*(-1).^(1:numel(xs));  % ばねのギザギザ具合の設定2
+            plot(xs,ys + 1.65,"k");        % ばねの描画(ばねの高さもオフセットして設定)
+            % ダンパの描画
+            yd = 0.5; % ダンパの高さ位置設定
+            xd = x0k + (xc-x0k)*0.5+0.1;
+            xd2 = x0k + (xc-x0k)*0.5;
+            xd3 = x0k + (xc-x0k)*0.5 + 0.5;
+            plot([x0k,xd,NaN,xd,xd],[yd,yd,NaN,yd-0.3,yd+0.3],"k")
+            plot([xd2,xd3,NaN,xd2,xd3,NaN,xd3,xd3,NaN,xd3,xc],[yd-0.5,yd-0.5,NaN,yd+0.5,yd+0.5,NaN,yd-0.5,yd+0.5,NaN,yd,yd],"k")
+            % 慣性体の位置を更新
+            x0k = xc + w;
+            % 慣性体の描画
+            x1 = spacing*k + x(j,k) - w/2;
+            rectangle("Position",[x1 y w h],"FaceColor","k")
+            % Plot x1, x2 distances
+            if (x(j,k) > 0)
+                quiver(spacing*k, y+h*1.1, x(j,k),0,"k-","MaxHeadSize",1/(x(j,k))^1,'AutoScale','off','LineWidth',3)
+            elseif (x(j,k) < 0)
+                quiver(spacing*k, y+h*1.1, x(j,k),0,"k-","MaxHeadSize",1/(-x(j,k))^1,'AutoScale','off','LineWidth',3)
+            else
+                quiver(spacing*k, y+h*1.1, 0,0,"k-","MaxHeadSize",1,'AutoScale','off','LineWidth',3)
+            end
+            %quiver(spacing*k, y+h*1.1, x(j,k),0,"k-","MaxHeadSize",1/(x(j,k))^1,'AutoScale','off','LineWidth',3,'ShowArrowHead','off')
+            plot([1;1]*[spacing*k, spacing*k+x(j,k)],[0.95;1.1]*(y*[1,1]+h*1.1),"k-","markersize",12)
+            text((spacing*k + spacing*k+x(j,k))/2,(y+h*1.3),"q(t)",'FontAngle', 'italic',"HorizontalAlignment","Center",'FontSize',25)
+            plot(spacing*k+x(j,k)-w/4,-0.25,'ko','MarkerSize',25,'MarkerFaceColor','k')
+            plot(spacing*k+x(j,k)+w/4,-0.25,'ko','MarkerSize',25,'MarkerFaceColor','k')
+        end
+        axis([0, xmax, -0.5, 3]) % 軸の範囲を変更
+        set(gca, "ytick", [])    % y軸消去
+        h_axes = gca;
+        h_axes.XAxis.FontSize = 22; % x軸フォントサイズの設定
+        h_axes.YAxis.FontSize = 22; % y軸フォントサイズの設定
+        tick_values1 = linspace(0, xmax, fix(xmax/5)+1);
+        tick_values2 = linspace(-spacing, spacing, fix(xmax/5)+1);
+        xticks(tick_values1);
+        xticklabels(arrayfun(@num2str, tick_values2, 'UniformOutput', false));
+        xlabel("Displacement [m]")
+        hold off
+        % シミュレーション結果の描画(慣性体の位置)
+        subplot(3,1,2) 
+        cla
+        hold on
+        plot(Time, Target,':k','LineWidth',4.0)
+        plot(t, x,'-b','LineWidth',4.0)
+        for k = 1:Nmass
+            plot(t(j), x(j, k), "bo", "MarkerFaceColor", 'b', 'MarkerSize', 10)
+        end
+        grid on;
+        h_axes = gca;
+        h_axes.XAxis.FontSize = 22; % x軸フォントサイズの設定
+        h_axes.YAxis.FontSize = 22; % y軸フォントサイズの設定
+        xlabel("Time [s]")
+        ylabel("Displacement [m]")
+        hold off
+        % シミュレーション結果の描画(制御入力)
+        subplot(3,1,3)
+        cla % サンプリングごとのマーカー履歴消去
+        hold on % グラフの保持
+        plot(t, u,'-b','LineWidth',4.0)
+        for k = 1:Nmass
+            plot(t(j), u(j, k), "bo", "MarkerFaceColor", 'b', 'MarkerSize', 10)
+        end
+        grid on;
+        h_axes = gca;
+        h_axes.XAxis.FontSize = 22; % x軸フォントサイズの設定
+        h_axes.YAxis.FontSize = 22; % y軸フォントサイズの設定
+        xlabel("Time [s]")
+        ylabel("Control input [N]")
+        hold off
+        % アニメーション& 動画処理
+        drawnow % アニメーションの確実な実行
+        writeVideo(vidObj, getframe(fig)); % フレームを動画に書き込む
+    end
+    close(vidObj); % 動画ファイルを閉じる
+end
---
+++ SET_Simulation_of_integral_servo_control.m
@@ -0,0 +1,60 @@
+%% 積分型サーボ制御のシミュレーション設定ファイル
+%% 1慣性系機械システム
+%% バージョン：MATLAB 2023b
+
+%% RESET
+clear variables; % 変数消去
+close all;       % Figure消去
+clc;             % コマンドウィンドウの履歴消去
+format compact;  % 余計な改行を消去
+%% SETTINGS SIMULATION
+% シミュレーション時間
+Sim_time = 10;         % シミュレーション時間
+Sampling_time = 0.001; % サンプリング時間
+% ステップ目標値
+Step_tarege = 10;      % ステップ値
+Target_rise_time = 1;  % 立ち上がり時間
+% 正弦波目標軌道
+Amplitude = 10;         % 振幅
+Angular_frequency = 3; % 各周波数
+Initial_phase = 0;     % 初期位相
+% ステップ外生入力
+Step_noise = 50;        % ステップ値
+Noise_rise_time = 5;   % 立ち上がり時間
+%% VARIABLE DEFINITION
+m = 1;  % 質量係数
+c = 2;  % 粘性係数
+k = 10; % ばね定数
+%% DEFINING STATE-SPACE MODEL
+A = [   0,    1 ;  % システム係数行列 A
+     -k/m, -c/m]; 
+B = [0; 1/m];      % 入力係数行列 B
+C = [1 0];         % 観測行列 C
+D = 0;             % 直達項 D
+x0 = zeros(2,1);   % 状態変数の初期値
+State_space = ss(A,B,C,D); % 状態空間モデルの定義
+n = length(A);     % 状態変数の数
+p = size(B,2);     % 制御入力(操作量)の数
+q = size(C,1);     % 制御出力(制御量)の数
+%% DEFINING EXTENDED STATE DEVIATION MODEL
+tildeAe = [ A, zeros(n,q);
+            -C, zeros(q,q)];
+tildeBe = [B;zeros(q,p)];
+%% CONTROLLER DESIGN
+q11 =    700; % 変位の重み
+q22 =    200; % 速度の重み
+q33 =  10000; % 積分器の重み
+Q = diag([q11 q22 q33]); % 状態変数重み行列の定義
+R   =   1;  % 入力重み行列の定義
+tildeKe = -lqr(tildeAe,tildeBe,Q,R);
+Ke = tildeKe(:,1:n);
+G  = tildeKe(:,n+1:n+q);
+%% SIMULATION
+open_system('Model_Simulation_of_integral_servo_control'); % Simulinkを起動
+sim('Model_Simulation_of_integral_servo_control'); % シミュレーション実行
+%% Animation
+Function_animate1MSD(Time,Displacement_Step,Target_Step,Control_Input_Step,(1/0.005))
+%Function_animate1MSD(Time,Displacement_SW,Target_SW,Control_Input_SW,(1/0.005))
+
+
+
